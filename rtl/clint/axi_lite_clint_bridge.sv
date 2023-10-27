07882cda534f146c88eeba8400164f6930760017
Alejandro Tafalla <atafalla@bsc.es>
2023-10-27 15:29:02 +0200

    fixup

--- rtl/clint/axi_lite_clint_bridge.sv
+++ rtl/clint/axi_lite_clint_bridge.sv
@@ -26,7 +26,7 @@ module axi_lite_clint_bridge #(
 
     // AXI Signals
     // address write
-    AXI_LITE.in axi,
+    AXI_LITE.Slave axi,
 
     output logic [    AXI_ADDR_WIDTH-1:0] address_o,
     output logic                          en_o,       // transaction is valid
