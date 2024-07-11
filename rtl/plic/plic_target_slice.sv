f8f969a9bca8508fb2705198f00dc2c548ca896f
“Javier <javier.salamero@bsc.es>
2024-07-11 12:08:24 +0200

    🐛 PLIC priority check fix

--- rtl/plic/plic_target_slice.sv
+++ rtl/plic/plic_target_slice.sv
@@ -25,9 +25,10 @@
 //-- Description: Target Slice
 //-------------------------------------------------------------------------------
 //-- Revisions  :
-//-- Date        Version  Author  Description
-//-- 2018-03-31  2.0      tbenz   Created header
+//-- Date        Version  Author   Description
+//-- 2018-03-31  2.0      tbenz    Created header
 //-- 2024-02-20  3.0      atafalla Refactoring, fix linting issues
+//-- 2024-02-206 3.1      atafalla Use HLib comparison pipeline selector with flush for comparing IRQ priorities
 //-------------------------------------------------------------------------------
 
 // Note: The gateways are expected to be ordered by their IDs (ascending).
@@ -35,73 +36,77 @@
 module plic_target_slice #(
     parameter int PRIORITY_BITWIDTH = 8,
     parameter int NUM_GATEWAYS      = 2,
-    localparam int IdBitwidth = $clog2(NUM_GATEWAYS + 1) // the +1 is because counting starts from 1 and goes to NUM_GATEWAYS+1
+    localparam int ID_BITWIDTH = $clog2(NUM_GATEWAYS + 1) // the +1 is because counting starts from 1 and goes to NUM_GATEWAYS+1
 ) (
     // Input signals from gateways.
-    input  logic                         clk_i,
-    input  logic                         rst_ni,
-    input  logic                         flush_cmp_pipeline_i,
-    input  logic                         interrupt_pending_i    [NUM_GATEWAYS],
-    input  logic [PRIORITY_BITWIDTH-1:0] interrupt_priority_i   [NUM_GATEWAYS],
-    input  logic [       IdBitwidth-1:0] interrupt_id_i         [NUM_GATEWAYS],
-    input  logic                         interrupt_enable_i     [NUM_GATEWAYS],
-    input  logic [PRIORITY_BITWIDTH-1:0] threshold_i,
-    output logic                         ext_interrupt_present_o,
-    output logic [       IdBitwidth-1:0] identifier_of_largest_o
+    input  logic                         clk_i,                                     //! clock input
+    input  logic                         rstn_i,                                    //! reset input, active low
+    input  logic                         flush_cmp_pipeline_i,                      //! flush comparison selector pipeline
+    input  logic                         interrupt_pending_i    [NUM_GATEWAYS-1:0], //! interrupt pending inputs, coming from peripherals
+    input  logic [PRIORITY_BITWIDTH-1:0] interrupt_priority_i   [NUM_GATEWAYS-1:0], //! interrupt priorities, coming from plic_interface registers
+    input  logic [ID_BITWIDTH-1:0]       interrupt_id_i         [NUM_GATEWAYS-1:0], //! interrupt IDs, static
+    input  logic                         interrupt_enable_i     [NUM_GATEWAYS-1:0], //! interrupt enables, coming from plic_interface registers
+    input  logic [PRIORITY_BITWIDTH-1:0] threshold_i,                               //! interrupt thresholds, coming from plic_interface registers
+    output logic                         ext_interrupt_present_o,                   //! external interrupt present
+    output logic [ID_BITWIDTH-1:0]       identifier_of_largest_o                    //! ID of the pending interrupt with highest priority
 );
-  logic [PRIORITY_BITWIDTH:0] interrupt_priority_masked[NUM_GATEWAYS];
+
+    logic [0:0] elegible_interrupt_vec [NUM_GATEWAYS-1:0];
+
+    logic [PRIORITY_BITWIDTH:0] interrupt_priority_masked[NUM_GATEWAYS-1:0];
 
 
-  // Signals that represent the selected interrupt source.
-  logic [PRIORITY_BITWIDTH:0] best_priority;
-  logic [     IdBitwidth-1:0] best_id;
-  logic                       best_valid;
+    // Signals that represent the selected interrupt source.
+    logic [PRIORITY_BITWIDTH:0] best_priority;
+    logic [ID_BITWIDTH-1:0]     best_id;
+    logic                       best_valid;
 
-  comparison_pipeline_selector_flush # (
-    .MTHAN_LTHAN(1'b1),
-    .DEFAULT_PRIORITY(1'b0),
-    .N_PORTS(NUM_GATEWAYS),
-    .ID_SIZE(IdBitwidth),
-    .DATA_WIDTH(PRIORITY_BITWIDTH + 1),
-    .REG_PATTERN({{($clog2(NUM_GATEWAYS)-1){1'b1}}, 1'b0})
-  ) find_max_prio (
-    .clk_i(clk_i),
-    .rstn_i(rst_ni),
+    comparison_pipeline_selector_flush #(
+        .MTHAN_LTHAN            (1'b1),
+        .DEFAULT_PRIORITY       (1'b0),
+        .N_PORTS                (NUM_GATEWAYS),
+        .ID_SIZE                (ID_BITWIDTH),
+        .DATA_WIDTH             (PRIORITY_BITWIDTH),
+        .REG_PATTERN            ({{($clog2(NUM_GATEWAYS)-1){1'b1}}, 1'b0})
+    ) find_max_prio (
+        .clk_i                  (clk_i),
+        .rstn_i                 (rstn_i),
 
-    .flush_i(flush_cmp_pipeline_i),
+        .flush_i                (flush_cmp_pipeline_i),
 
-    .comp_en_i(interrupt_pending_i),
-    .comparation_regs_i(interrupt_priority_masked),
-    .ids_of_regs_i(interrupt_id_i),
+        .comp_en_i              (elegible_interrupt_vec),
+        .comparation_regs_i     (interrupt_priority_i),
+        .ids_of_regs_i          (interrupt_id_i),
 
-    .valid_o(best_valid),
-    .selected_value_o(best_priority),
-    .selected_id_o(best_id)
-  );
+        .valid_o                (best_valid),
+        .selected_value_o       (best_priority),
+        .selected_id_o          (best_id)
+    );
 
-  // Compare the priority of the best interrupt source to the threshold.
-  always_comb begin : proc_compare_threshold
-    if (((best_priority - 1) > threshold_i) && (best_priority != '0)) begin
-      ext_interrupt_present_o = best_valid;
-      identifier_of_largest_o = best_id;
-    end else begin
-      if (((best_priority - 1) <= threshold_i) && (best_priority != '0)) begin
-        ext_interrupt_present_o = 0;
-        identifier_of_largest_o = best_id;
-      end else begin
-        ext_interrupt_present_o = 0;
-        identifier_of_largest_o = 0;
-      end
+    // Compare the priority of the best interrupt source to the threshold.
+    always_comb begin : proc_compare_threshold
+        if ((best_priority > threshold_i) && best_valid) begin
+            ext_interrupt_present_o = best_valid;
+            identifier_of_largest_o = best_id;
+        end else begin
+            if ((best_priority <= threshold_i) && best_valid) begin // TODO: why does it matter if it is not valid anyways ??
+                ext_interrupt_present_o = 0;
+                identifier_of_largest_o = best_id;
+            end else begin
+                ext_interrupt_present_o = 0;
+                identifier_of_largest_o = 0;
+            end
+        end
     end
-  end
 
-  always_comb begin : proc_mask_gateway_outputs
-    for (int i = 0; i < NUM_GATEWAYS; i++) begin
-      if (interrupt_enable_i[i] && interrupt_pending_i[i]) begin
-        interrupt_priority_masked[i] = interrupt_priority_i[i] + 1;  //priority shift +1
-      end else begin
-        interrupt_priority_masked[i] = '0;
-      end
+    always_comb begin : proc_mask_gateway_outputs
+        for (int i = 0; i < NUM_GATEWAYS; i++) begin
+            elegible_interrupt_vec[i] = interrupt_enable_i[i] & interrupt_pending_i[i];
+            // if (interrupt_enable_i[i] && interrupt_pending_i[i]) begin
+            //     interrupt_priority_masked[i] = interrupt_priority_i[i] + 1;  //priority shift +1
+            // end else begin
+            //     interrupt_priority_masked[i] = '0;
+            // end
+        end
     end
-  end
 endmodule
